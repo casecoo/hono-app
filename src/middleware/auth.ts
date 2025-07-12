333e64b66c3d8b6db094f15af5f62fc16ded46f4
Erman <ermanozbay25@gmail.com>
2025-07-12 17:22:38 +0300

    Update auth.ts

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -6,8 +6,6 @@ import { checkAuth } from '../utils/auth-utils'
 
 export async function authMiddleWare(c: Context, next: Next) {
     
-    const userId = '120'
-    c.set('userId',userId)
     const isAuthorized = checkAuth(c)
     if (!isAuthorized) return c.text('Unauthorized access!', 401)
     await next()
